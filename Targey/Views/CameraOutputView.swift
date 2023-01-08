2a811509b48ce48525d9e4d04027eaa08d9a2177
Jaylen Smith <kobe26945@icloud.com>
2023-01-07 23:28:48 -0500

    Added

---
+++ Targey/Views/CameraOutputView.swift
@@ -0,0 +1,29 @@
+//
+//  CameraOutputView.swift
+//  Targey
+//
+//  Created by Jaylen Smith on 1/7/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct CameraOutputView: View {
+    
+    @State private var recWidth: CGFloat = UIScreen.main.bounds.width-100
+    @State private var recHeight: CGFloat = 200
+    
+    @ObservedObject var cameraModel: CameraModel
+    
+    var body: some View {
+        ZStack {
+            CameraSession(cameraModel: cameraModel)
+                 .overlay {
+                     RoundedRectangle(cornerRadius: 15)
+                         .stroke(.white)
+                         .frame(width: recWidth, height: recHeight)
+                 }
+        }
+        .ignoresSafeArea()
+    }
+}
