0f30884f8cb9d5da86b8eb4b3cc5c6910eb62cfe
Jaylen Smith <kobe26945@icloud.com>
2023-01-10 13:10:36 -0500

    Implemented Promotions Manager

---
+++ Targey/Firebase/PromotionsManager.swift
@@ -0,0 +1,20 @@
+//
+//  PromotionsManager.swift
+//  Targey
+//
+//  Created by Jaylen Smith on 1/10/23.
+//
+
+import Foundation
+
+
+enum PMFetchError: Error {
+    case error(String)
+}
+
+final class PromotionsManager {
+    
+    func fetchAllPromotions(completion: (Result<[Promotion], PMFetchError>)) {
+        
+    }
+}
