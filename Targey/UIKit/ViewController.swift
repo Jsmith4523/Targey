07fa8fbcba910d1412cfdcd0af0eaa9afebdff10
Jaylen Smith <jaylensmith4521@gmail.com>
2023-01-12 23:48:29 -0500

    Adding in UiKit folder

---
+++ Targey/UIKit/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Targey
+//
+//  Created by Jaylen Smith on 1/12/23.
+//
+
+import UIKit
+
+class ViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
